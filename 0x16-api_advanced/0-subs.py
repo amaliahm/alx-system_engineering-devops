42374254a74c1f8f9268f1376cb767276893382a
amaliahm <a.ghandouz@esi-sba.dz>
2024-08-14 19:16:31 +0100

    fix 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,19 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    data = response.json()
+    subscribers = data.get("data", {}).get("subscribers", 0)
+    return subscribers
+
+
+if __name__ == "__main__":
+    subreddit = "programming"
+    print(f"Subscribers: {number_of_subscribers(subreddit)}")
